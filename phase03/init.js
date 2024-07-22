f32eade753a63b0a54727192541f11eebbed9b4f
Mojtaba Erfan Rad <mojmal_mojar@yahoo.com>
2024-07-22 17:14:41 +0330

    finished phase 03

---
+++ phase03/init.js
@@ -0,0 +1,37 @@
+async function fetchData() {
+    const url = 'https://raw.githubusercontent.com/Star-Academy/codestar-documents/master/static/datasets/books.json';
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Failed to fetch the JSON file:', error);
+    }
+}
+
+async function renderBooks () {
+    const data = await fetchData();
+    const article = document.querySelector('article');
+    data.forEach(element => {
+        const book = document.createElement('div');
+        const title = document.createElement('h3');
+        const author = document.createElement('p');
+        const image = document.createElement('img');
+
+        title.textContent = element.name;
+        author.textContent = element.author;
+        image.src = element.image;
+        
+        book.appendChild(image);
+        book.appendChild(title);
+        book.appendChild(author);
+
+        article.appendChild(book);
+
+    });
+}
+
+renderBooks ()
